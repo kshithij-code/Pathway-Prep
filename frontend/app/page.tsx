ce4e0222a42ace58ddb69b592ab6ed932599e158
kshithij <kshithijy@gmail.com>
2024-11-21 21:59:32 +0530

    init

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import VideoConferencing from "./meet/page";
 import PeerMessaging from "./peer-messaging/page";
+import { TechnicalInterviewQuiz } from "./onboarding_quiz/page";
 
 export default function Home() {
   return (
     <div>
-      <PeerMessaging></PeerMessaging>
+      <TechnicalInterviewQuiz/>
     </div>
   );
 }
